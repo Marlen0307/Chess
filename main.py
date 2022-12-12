d1df44873affe615926ecd23d977a81bcabf29a8
Marlen Hima <marlen.hima001@stud.fh-dortmund.de>
2022-12-12 23:40:51 +0100

    feat: add Game class

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-from models.ChessBoard import ChessBoard
-from models.Player import Player
-
-players = [] * 2
-colors = {
-    0: 'white',
-    1: 'black'
-}
-
-board = ChessBoard()
-
-
-def init_players():
-    for number in [0, 1]:
-        num_of_player = number + 1
-        default_player_name = "Player " + str(num_of_player)
-        player_name = input("Write name for " + default_player_name)
-        if player_name == "":
-            player_name = default_player_name
-        player_color = colors[number]
-        players.insert(number, Player(number, player_name, player_color))
+from models.Game import Game
 
 
 def start_game():
-    print(board)
-    init_players()
-    print(players)
+    game = Game()
+    print(game)
 
 
 start_game()
