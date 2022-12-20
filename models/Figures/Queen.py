5f22013b89f80bb8f0f021a70a34babce2595c49
Marlen Hima <marlen.hima001@stud.fh-dortmund.de>
2022-12-20 16:56:17 +0100

    feat: queen movement

--- models/Figures/Queen.py
+++ models/Figures/Queen.py
@@ -1,7 +1,28 @@
+from Constants import Directions, CrossDirections
 from models import Postion
 from models.ChessFigure import ChessFigure
+from models.Figures.Bishop import get_top_right_left_bottom_moves, get_top_left_right_bottom_moves
+from models.Figures.Rook import get_horizontal_moves, get_vertical_moves
 
 
 class Queen(ChessFigure):
     def __init__(self, color: str, title: str, position: Postion, direction, player):
         super().__init__(color, title, position, direction, player)
+
+    def get_all_moves(self):
+        x = self.position.x
+        y = self.position.y
+        left_positions = get_horizontal_moves(self, x, y, Directions.LEFT)
+        right_positions = get_horizontal_moves(self, x, y, Directions.RIGHT)
+        top_positions = get_vertical_moves(self, x, y, Directions.TOP)
+        bottom_positions = get_vertical_moves(self, x, y, Directions.BOTTOM)
+        top_right_moves = get_top_right_left_bottom_moves(self, self.position, CrossDirections.RIGHT_TOP)
+        bottom_left_moves = get_top_right_left_bottom_moves(self, self.position, CrossDirections.LEFT_BOTTOM)
+        top_left_moves = get_top_left_right_bottom_moves(self, self.position, CrossDirections.LEFT_TOP)
+        right_bottom_moves = get_top_left_right_bottom_moves(self, self.position, CrossDirections.RIGHT_BOTTOM)
+        return left_positions + right_positions + top_positions + bottom_positions + top_right_moves + top_left_moves +\
+            bottom_left_moves + right_bottom_moves
+
+    def get_moving_options(self):
+        moves = self.get_all_moves()
+        self.get_next_move(moves)
--- models/Figures/Rook.py
+++ models/Figures/Rook.py
@@ -4,71 +4,71 @@ from models import Postion
 from Constants import Directions, extremes
 
 
+def get_horizontal_moves(figure, x, y,
+                         direction):
+    index_of_coordinate_to_be_moved = get_x_index(x)
+
+    def get_condition():
+        condition = index_of_coordinate_to_be_moved >= extremes[direction]
+
+        if direction == Directions.RIGHT:
+            condition = index_of_coordinate_to_be_moved <= extremes[direction]
+        return condition
+
+    moves_arr = []
+    while get_condition():
+        pos = get_position(ChessBoard.x_positions[index_of_coordinate_to_be_moved], y)
+        if is_position_valid(figure, pos):
+            moves_arr.append(pos)
+            index_of_coordinate_to_be_moved = index_of_coordinate_to_be_moved + figure.get_direction(direction) * 1
+            if pos.occupied:  # if we come here a enemy is in this position which means that is the last valid position in that direction so we break the loop
+                break
+            continue
+
+        index_of_coordinate_to_be_moved = index_of_coordinate_to_be_moved + figure.get_direction(direction) * 1
+        break_condition = pos.to_string() != figure.position.to_string()
+        if break_condition:
+            break
+    return moves_arr
+
+
+def get_vertical_moves(figure, x, y, direction):
+    figure.direction = direction
+
+    index_of_coordinate_to_be_moved = get_y_index(y)
+
+    def get_condition():
+        condition = index_of_coordinate_to_be_moved >= extremes[direction]
+
+        if direction == Directions.TOP:
+            condition = index_of_coordinate_to_be_moved <= extremes[direction]
+        return condition
+
+    moves_arr = []
+    while get_condition():
+        pos = get_position(x, ChessBoard.y_positions[index_of_coordinate_to_be_moved])
+        if is_position_valid(figure, pos):
+            moves_arr.append(pos)
+            index_of_coordinate_to_be_moved = index_of_coordinate_to_be_moved + figure.get_direction(direction) * 1
+            continue
+        index_of_coordinate_to_be_moved = index_of_coordinate_to_be_moved + figure.get_direction(direction) * 1
+        break_condition = pos.to_string() != figure.position.to_string()
+        if break_condition:
+            break
+    return moves_arr
+
+
 class Rook(ChessFigure):
     def __init__(self, color: str, title: str, position: Postion, direction, player):
         super().__init__(color, title, position, direction, player)
 
-    def get_horizontal_moves(self, x, y,
-                             direction):
-
-        index_of_coordinate_to_be_moved = get_x_index(x)
-
-        def get_condition():
-            condition = index_of_coordinate_to_be_moved >= extremes[direction]
-
-            if direction == Directions.RIGHT:
-                condition = index_of_coordinate_to_be_moved <= extremes[direction]
-            return condition
-
-        moves_arr = []
-        while get_condition():
-            pos = get_position(ChessBoard.x_positions[index_of_coordinate_to_be_moved], y)
-            if is_position_valid(self, pos):
-                moves_arr.append(pos)
-                index_of_coordinate_to_be_moved = index_of_coordinate_to_be_moved + self.get_direction(direction) * 1
-                if pos.occupied:  # if we come here a enemy is in this position which means that is the last valid position in that direction so we break the loop
-                    break
-                continue
-
-            index_of_coordinate_to_be_moved = index_of_coordinate_to_be_moved + self.get_direction(direction) * 1
-            break_condition = pos.to_string() != self.position.to_string()
-            if break_condition:
-                break
-        return moves_arr
-
-    def get_vertical_moves(self, x, y, direction):
-
-        self.direction = direction
-
-        index_of_coordinate_to_be_moved = get_y_index(y)
-
-        def get_condition():
-            condition = index_of_coordinate_to_be_moved >= extremes[direction]
-
-            if direction == Directions.TOP:
-                condition = index_of_coordinate_to_be_moved <= extremes[direction]
-            return condition
-
-        moves_arr = []
-        while get_condition():
-            pos = get_position(x, ChessBoard.y_positions[index_of_coordinate_to_be_moved])
-            if is_position_valid(self, pos):
-                moves_arr.append(pos)
-                index_of_coordinate_to_be_moved = index_of_coordinate_to_be_moved + self.get_direction(direction) * 1
-                continue
-            index_of_coordinate_to_be_moved = index_of_coordinate_to_be_moved + self.get_direction(direction) * 1
-            break_condition = pos.to_string() != self.position.to_string()
-            if break_condition:
-                break
-        return moves_arr
-
     def get_all_moves(self):
         x = self.position.x
         y = self.position.y
-        left_positions = self.get_horizontal_moves(x, y, Directions.LEFT)
-        right_positions = self.get_horizontal_moves(x, y, Directions.RIGHT)
-        top_positions = self.get_vertical_moves(x, y, Directions.TOP)
-        bottom_positions = self.get_vertical_moves(x, y, Directions.BOTTOM)
+        left_positions = get_horizontal_moves(self, x, y, Directions.LEFT)
+        right_positions = get_horizontal_moves(self, x, y, Directions.RIGHT)
+        top_positions = get_vertical_moves(self, x, y, Directions.TOP)
+        bottom_positions = get_vertical_moves(self, x, y, Directions.BOTTOM)
         return left_positions + right_positions + top_positions + bottom_positions
 
     def get_moving_options(self):
